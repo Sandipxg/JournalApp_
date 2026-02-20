72a77e089c743e4f2bc44529799b29c6bf7c13da
Sandip <mrsandipgodhani@gmail.com>
2026-02-20 18:30:49 +0530

    basic syntaxs learnign

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+
+const myelement = (
+  <h1> Hellow world </h1> 
+  
+);
+
+class user{
+  constructor(name){
+    this.name = name;
+  }
+
+  present(){
+    return "hello " + this.name;
+  }
+
+  hello = () => {
+    return "hello brooooooo"
+  }
+}
+
+var nm = new user("Sandip")
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <>
+  {myelement}
+  {nm.present()}
+  {nm.hello()}
+  </>
+  
+)  
